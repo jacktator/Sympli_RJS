b3f853d42f4dd2c670b13ca60b48cdbe84111cce
Jack Song <jack.song@lendi.com.au>
2021-02-01 13:28:30 +1100

    - Added `styled-components` - Configure Storybook

---
+++ app/src/components/GenericError/index.stories.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import {GenericError as Component} from './'
+
+export default {
+  title: 'App/Components',
+}
+
+export const GenericError = () => <Component />
--- storybook/.storybook/main.js
+++ storybook/.storybook/main.js
@@ -1,7 +1,9 @@
 module.exports = {
   "stories": [
-    "../../app/src/**/*.stories.mdx",
-    "../../app/src/**/*.stories.@(js|jsx|ts|tsx)"
+    "../../app/**/*.stories.mdx",
+    "../../app/**/*.stories.@(js|jsx|ts|tsx)",
+    "../../packages/**/src/*.stories.mdx",
+    "../../packages/**/src/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
     "@storybook/addon-links",
