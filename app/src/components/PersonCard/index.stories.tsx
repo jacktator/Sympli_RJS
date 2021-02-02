af1f9ed60109d2a1daa5bbc81fccea668a7fa479
Jack Song <jack.song@lendi.com.au>
2021-02-03 06:23:47 +1100

    Fixed Story

--- app/src/components/PersonCard/index.stories.tsx
+++ app/src/components/PersonCard/index.stories.tsx
@@ -2,11 +2,13 @@ import * as React from "react";
 import {PersonCard as Component, PersonCardProps} from "./";
 import {PERSON} from "./__mock__";
 import {MemoryRouter} from "react-router-dom";
+import {Meta} from '@storybook/react/types-6-0';
 
-export default {
+const meta: Meta = {
   title: "App/Components",
   decorators: [(Story) => <MemoryRouter><Story/></MemoryRouter>]
 };
+export default meta;
 
 export const PersonCard = (args: PersonCardProps) => <Component {...args} />;
 PersonCard.args = {
--- app/src/containers/PeopleContainer/index.stories.tsx
+++ app/src/containers/PeopleContainer/index.stories.tsx
@@ -1,11 +1,13 @@
 import * as React from "react";
 import * as Container from './'
 import {MemoryRouter} from "react-router-dom";
+import {Meta} from '@storybook/react/types-6-0';
 
-export default {
+const meta: Meta = {
   title: 'App/Containers',
   decorators: [(Story) => <MemoryRouter><Story/></MemoryRouter>]
 }
+export default meta;
 
 export const PeopleContainer = () => <Container.default />
 PeopleContainer.args = {
