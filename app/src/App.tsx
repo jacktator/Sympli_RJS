486fedb37e648777b8d49d77eed565f7e78a0493
Jack Song <jack.song@lendi.com.au>
2021-02-03 06:41:44 +1100

    Implemented Theme

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -4,18 +4,30 @@ import {BrowserRouter as Router, Redirect, Route, Switch} from "react-router-dom
 import {Loading} from "./components/Loading";
 import {PATH_PEOPLE} from "./utils/constants";
 import {Navbar} from "./components/Navbar";
+import {createMuiTheme, ThemeProvider} from "@material-ui/core";
+import {themeOptions} from "./theme";
 
 const Home = lazy(() => import('./page/Home'));
+
+const theme = createMuiTheme(themeOptions);
 
 const App = () => {
   return (
     <ErrorBoundary>
       <Suspense fallback={<Loading />}>
-        <AppWithRouter />
+        <AppWithTheme />
       </Suspense>
     </ErrorBoundary>
   );
 };
+
+const AppWithTheme = () => {
+  return (
+    <ThemeProvider theme={theme}>
+      <AppWithRouter />
+    </ThemeProvider>
+  )
+}
 
 const AppWithRouter = () => {
   return (
---
+++ app/src/theme.ts
@@ -0,0 +1,16 @@
+import {ThemeOptions} from '@material-ui/core/styles/createMuiTheme';
+
+export const themeOptions: ThemeOptions = {
+  palette: {
+    type: 'light',
+    primary: {
+      main: '#212121',
+    },
+    secondary: {
+      main: '#ffea00',
+    },
+    text: {
+      primary: '#212121',
+    },
+  },
+};
